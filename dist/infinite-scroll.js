e98aa894b3d48c492d4462456347fb8701179506
Cody <callahanrts@gmail.com>
2016-02-21 09:44:40 -0800

    tests for using the window as the scroll container

--- dist/infinite-scroll.js
+++ dist/infinite-scroll.js
@@ -1,18 +1,66 @@
 (function(){
+  var InfiniteScrollController = function($scope, $q, $timeout){
+    this.elementHeight = 0;
+    var vm = this;
+
+    this.loadOnScroll = function(el, dist){
+      angular.element(window).bind("scroll", function(){
+        if(scrollTop(el) + viewportHeight(el) > scrollHeight(el) - viewportHeight(el) * (dist / 100)){
+          vm.loadData(el);
+        }
+      });
+    };
+
+    var scrollTop = function(el){
+      return document.body.scrollTop;
+    };
+
+    var scrollHeight = function(el){
+      return el.scrollHeight;
+    };
+
+    var viewportHeight = function(){
+      return screen.height;
+    };
+
+    this.loadData = function(el){
+      $q.when($scope.loadMore(), function(){
+        $timeout(function(){
+          var height = el.clientHeight;
+          // Load more data if the window is inable to scroll and there is more
+          // data to load (height has changed)
+          if(window.innerHeight > height && height != vm.elementHeight){
+            vm.elementHeight = height;
+            vm.loadData(el);
+          }
+        }, 0)
+      });
+    };
+
+  };
+
+  InfiniteScrollController.$inject = ['$scope', '$q', '$timeout'];
 
   var infiniteScroll = function(){
-    link = function(scope, element, attrs){
-      console.log(arguments);
-      console.log(attrs.infiniteScroll);
-      scope.infiniteScroll()
-      //attrs.infiniteScroll(5);
+    link = function(scope, el, attrs, ctrl){
+      // Set Defaults
+      if(!angular.isNumber(parseInt(scope.scrollDistance))){
+        scope.scrollDistance = 10;
+      }
+
+      // Get Data
+      ctrl.loadData(el[0]);
+      ctrl.loadOnScroll(el[0], scope.scrollDistance);
     };
 
     return {
       scope: {
-        infiniteScroll: "&"
+        loadMore: "&",
+        scrollDistance: "="
       },
-      //restrict: "E",
+      controller: InfiniteScrollController,
+      controllerAs: 'vm',
+      restrict: "EA",
       link: link
     };
   };
