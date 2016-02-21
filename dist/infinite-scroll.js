86b82bbf06d384b87e5272876d37e6231aff5738
Cody <callahanrts@gmail.com>
2016-02-21 13:06:23 -0800

    tests for container element

--- dist/infinite-scroll.js
+++ dist/infinite-scroll.js
@@ -1,18 +1,20 @@
 (function(){
   var InfiniteScrollController = function($scope, $q, $timeout){
     this.elementHeight = 0;
+    this.container = window; // default container to window
     var vm = this;
 
     this.loadOnScroll = function(el, dist){
-      angular.element(window).bind("scroll", function(){
-        if(scrollTop(el) + viewportHeight(el) > scrollHeight(el) - viewportHeight(el) * (dist / 100)){
+      console.log(vm.container, el, dist)
+      angular.element(vm.container).bind("scroll", function(){
+        if(scrollTop() + viewportHeight(el) > scrollHeight(el) - viewportHeight(el) * (dist / 100)){
           vm.loadData(el);
         }
       });
     };
 
-    var scrollTop = function(el){
-      return document.body.scrollTop;
+    var scrollTop = function(){
+      return vm.container == window ? document.body.scrollTop : vm.container.scrollTop;
     };
 
     var scrollHeight = function(el){
@@ -20,7 +22,12 @@
     };
 
     var viewportHeight = function(){
-      return screen.height;
+      return vm.container == window ? screen.height : vm.container.clientHeight;
+    };
+
+    var ableToScroll = function(el){
+      var scrollHeight = (vm.container == window ? window.innerHeight : vm.container.scrollHeight)
+      return scrollHeight <= el.clientHeight;
     };
 
     this.loadData = function(el){
@@ -29,7 +36,7 @@
           var height = el.clientHeight;
           // Load more data if the window is inable to scroll and there is more
           // data to load (height has changed)
-          if(window.innerHeight > height && height != vm.elementHeight){
+          if(!ableToScroll(el) && height != vm.elementHeight){
             vm.elementHeight = height;
             vm.loadData(el);
           }
@@ -44,23 +51,28 @@
   var infiniteScroll = function(){
     link = function(scope, el, attrs, ctrl){
       // Set Defaults
-      if(!angular.isNumber(parseInt(scope.scrollDistance))){
+      dist = parseInt(scope.scrollDistance);
+      if(!angular.isNumber(dist)){
         scope.scrollDistance = 10;
       }
 
       // Get Data
+      if(angular.isString(scope.scrollContainer)){
+        ctrl.container = document.getElementById(scope.scrollContainer);
+      }
       ctrl.loadData(el[0]);
-      ctrl.loadOnScroll(el[0], scope.scrollDistance);
+      ctrl.loadOnScroll(el[0], dist);
     };
 
     return {
       scope: {
         loadMore: "&",
-        scrollDistance: "="
+        scrollDistance: "=",
+        scrollContainer: "="
       },
       controller: InfiniteScrollController,
       controllerAs: 'vm',
-      restrict: "EA",
+      restrict: "AEC",
       link: link
     };
   };
---
+++ test/infinite-scroll.js
@@ -0,0 +1,175 @@
+// ///////////////////////////////////////////
+// Window.js
+
+var load = function(options){
+  options = options || {}
+  var href = "test/templates/"+options.container+".html?count="+options.count;
+  if(options.total > 0){ href += "&total="+options.total; }
+  if(options.scroll > 0){ href += "&dist="+options.scroll; }
+  browser.get(href);
+};
+
+var lengthIs = function(length){
+  element.all(by.css("#list > *")).then(function(items){
+    expect(items.length).toBe(length);
+  });
+};
+
+var containerCanScroll = function(){
+  var container = element(by.id('container'));
+  container.getAttribute('height').then(function (height) {
+      expect(container.getAttribute('scrollHeight')).toBeGreaterThan(height);
+  });
+};
+
+var windowCanScroll = function(){
+  var list = element(by.id('list'));
+  browser.executeScript('return window.innerHeight;').then(function(winHeight){
+    list.getSize().then(function(size){
+      expect(size.height > winHeight).toBe(true);
+    });
+  });
+};
+
+var scrollBody = function(percentage){
+  // Scroll percentage defaults to 10%
+  percentage = percentage || 10;
+
+  var script = function(percentage){
+    setTimeout(function(){
+      var scrollPos = 1 + document.body.scrollHeight - screen.height * (1 + percentage / 100);
+      window.scrollTo(0, scrollPos);
+    }, 0)
+  }
+  return browser.executeScript(script, percentage);
+};
+
+var scrollContainer = function(percentage){
+  var percentage = percentage || 10;
+
+  var script = function(percentage){
+    setTimeout(function(){
+      var container = document.getElementById('container');
+      var scrollPos = 1 + container.scrollHeight - container.clientHeight * (1 + percentage / 100);
+      container.scrollTop = scrollPos;
+    }, 0)
+  };
+  return browser.executeScript(script, percentage);
+};
+
+describe('#InfiniteScroll', function(){
+
+  describe('when testing', function(){
+    it('should limit by total', function(){
+      load({ container: "window", count: 100, total: 10 });
+      lengthIs(10);
+    });
+  });
+
+  describe('when the scroll container is specified in the directive\n', function() {
+    afterEach(function(){
+      browser.executeScript("document.getElementById('container').scrollTop = 0");
+    });
+
+    it('should load data initially', function() {
+      load({ container: "container", count: 10, total: 10 });
+      lengthIs(10);
+    });
+
+    it('should load more items until the page can scroll', function(){
+      load({ container: "container", count: 10 });
+      containerCanScroll();
+    });
+
+    describe('when there are no more items to load\n', function(){
+      describe('when there are enough items to scroll\n', function(){
+        it('should stop loading items', function(){
+          load({ container: "container", count: 100, total: 200, scroll: 10 });
+          scrollContainer(10).then(function(){
+            lengthIs(200);
+          });
+        });
+      });
+
+      describe('when there are not enough items to scroll\n', function(){
+        it('should stop loading items', function(){
+          load({ container: "container", count: 5, total: 12 });
+          lengthIs(12);
+        });
+      });
+
+    });
+
+    describe('when the scroll event is triggered\n', function(){
+      it('should load more data', function(){
+        load({ container: "container", count: 100, total: 1000, scroll: 10 });
+        scrollContainer(10).then(function(){
+          lengthIs(200);
+        });
+      });
+
+      describe('when the scroll distance is 50%\n', function(){
+        it('should load moare data', function(){
+          load({ container: "container", count: 100, total: 1000, scroll: 50 });
+          scrollContainer(50).then(function(){
+            lengthIs(200);
+          });
+        });
+      });
+    });
+
+  });
+
+  describe('when the scroll container is the window\n', function() {
+    afterEach(function(){
+      browser.executeScript("window.scrollTo(0,0)");
+    });
+
+    it('should load data initially', function() {
+      load({ container: "window", count: 10, total: 10 });
+      lengthIs(10);
+    });
+
+    it('should load more items until the page can scroll', function(){
+      load({ container: "window", count: 10 });
+      windowCanScroll();
+    });
+
+    describe('when there are no more items to load\n', function(){
+      describe('when there are enough items to scroll\n', function(){
+        it('should stop loading items', function(){
+          load({ container: "window", count: 100, total: 200, scroll: 10 });
+          scrollBody(10, function(){
+            lengthIs(200);
+          });
+        });
+      });
+
+      describe('when there are not enough items to scroll\n', function(){
+        it('should stop loading items', function(){
+          load({ container: "window", count: 10, total: 12 });
+          lengthIs(12);
+        });
+      });
+    });
+
+    describe('when the scroll event is triggered\n', function(){
+      it('should load more data', function(){
+        load({ container: "window", count: 100, total: 1000, scroll: 10 });
+        scrollBody(10).then(function(){
+          lengthIs(200);
+        });
+      });
+
+      describe('when the scroll distance is 50%\n', function(){
+        it('should load moare data', function(){
+          load({ container: "window", count: 100, total: 1000, scroll: 50 });
+          scrollBody(50).then(function(){
+            lengthIs(200);
+          });
+        });
+      });
+    });
+
+  });
+});
