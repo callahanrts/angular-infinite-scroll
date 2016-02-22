3da6742b1402d44a36133cb2556311e775128b9f
Cody <callahanrts@gmail.com>
2016-02-22 11:52:24 -0800

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,11 @@
 npm run uglify
 git add -A
 git commit -m "minify"
-npm run tag $0
+
+if [ -z "$1" ];then
+  git push origin master
+else
+  npm run tag $1
+  git push origin master --tags
+fi
+
--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 git tag -a v$1 -m "Release version $1"
-git push origin master --tags
